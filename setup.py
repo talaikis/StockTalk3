215afb14370f42b651cd77f6a6793a36358706d7
Tadas Talaikis <xenu256@users.noreply.github.com>
2017-03-05 13:00:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='stocktalk3',
+      version='v1.0',
+      description='Data collection toolkit for social media analytics',
+      author='Tadas Talaikis',
+      author_email='talaikis.tadas@gmail.com',
+      url='https://github.com/xenu256/StockTalk3',
+      packages=['stocktalk3'],
+      scripts=['bin/stocktalk-corpus']
+     )
